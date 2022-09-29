c01c8507f23ddea6239b5d1d53ddd0c0c2ff17b3
Hengky <hengkysurya88@gmail.com>
2022-09-30 00:17:57 +0700

    feat(auth-app): add endpoint to register new user
    Add endpoint to register new user. Validate if a user already registered by same phone number. User data saved to the database.

--- auth-app/app.js
+++ auth-app/app.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 var pingRouter = require("./routes/ping");
+var userRouter = require("./routes/user");
 
 const app = express()
 
 app.use(express.json());
 
-// register routes here
+/* register routes here */
 app.use("/ping", pingRouter);
+
+// User Domain
+app.use("/user", userRouter)
 
 module.exports = app;
